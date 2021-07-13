be655255f8f214c0a678926662c906dda35efd67
Akshay <cakshay001@gmail.com>
2021-07-13 13:00:36 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,40 @@
-import { useState, useEffect } from 'react'
-import { Navigation } from './components/navigation'
-import { Header } from './components/header'
-import { Features } from './components/features'
-import { About } from './components/about'
-import { Services } from './components/services'
-import { Gallery } from './components/gallery'
-import { Testimonials } from './components/testimonials'
-import { Team } from './components/Team'
-import { Contact } from './components/contact'
-import JsonData from './data/data.json'
-import SmoothScroll from 'smooth-scroll'
-
+import { useState, useEffect } from "react";
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
+import { Features } from "./components/features";
+import { About } from "./components/about";
+import { Services } from "./components/services";
+import { Gallery } from "./components/gallery";
+import { Testimonials } from "./components/testimonials";
+import { Team } from "./components/Team";
+import { Contact } from "./components/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import Darkmode from "darkmode-js";
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
-})
+});
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({})
+  const [landingPageData, setLandingPageData] = useState({});
   useEffect(() => {
-    setLandingPageData(JsonData)
-  }, [])
-
+    setLandingPageData(JsonData);
+  }, []);
+  new Darkmode().showWidget();
   return (
     <div>
       <Navigation />
       <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
+      {/* <Features data={landingPageData.Features} /> */}
       <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
+      {/* <Services data={landingPageData.Services} /> */}
       <Gallery />
-      <Testimonials data={landingPageData.Testimonials} />
+      {/* <Testimonials data={landingPageData.Testimonials} /> */}
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,18 +1,18 @@
 export const About = (props) => {
   return (
-    <div id='about'>
-      <div className='container'>
-        <div className='row'>
-          <div className='col-xs-12 col-md-6'>
-            {' '}
-            <img src='img/about.jpg' className='img-responsive' alt='' />{' '}
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
           </div>
-          <div className='col-xs-12 col-md-6'>
-            <div className='about-text'>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
               <h2>About Us</h2>
-              <p>{props.data ? props.data.paragraph : 'loading...'}</p>
-              <h3>Why Choose Us?</h3>
-              <div className='list-style'>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              {/* <h3>Why Choose Us?</h3> */}
+              {/* <div className='list-style'>
                 <div className='col-lg-6 col-sm-6 col-xs-12'>
                   <ul>
                     {props.data
@@ -31,11 +31,11 @@ export const About = (props) => {
                       : 'loading'}
                   </ul>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
